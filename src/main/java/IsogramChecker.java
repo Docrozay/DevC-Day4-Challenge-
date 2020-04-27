04c1d6fc1903767d6ee366a72bb6ea23a81cd2e1
Roosevelt <42326422+Docrozay@users.noreply.github.com>
2020-04-27 20:50:26 +0100

    Initial commit

---
+++ src/main/java/IsogramChecker.java
@@ -0,0 +1,16 @@
+
+
+class IsogramChecker {
+
+    /**
+     * Determine if a word or phrase is an isogram.
+     *
+     * @param phrase string variable
+     * @return True or False
+     */
+
+    boolean isIsogram(String phrase) {
+
+        return true;
+    }
+}
